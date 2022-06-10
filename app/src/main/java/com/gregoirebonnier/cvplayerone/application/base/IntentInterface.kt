754e13165cda6e9bc61473848534b6559725d95c
Grégoire Bonnier <31819901+gregbnr@users.noreply.github.com>
2022-06-10 13:17:19 +0200

    Add files via upload

---
+++ app/src/main/java/com/gregoirebonnier/cvplayerone/application/base/IntentInterface.kt
@@ -0,0 +1,16 @@
+package com.gregoirebonnier.cvplayerone.application.base
+
+/**
+ * the current state of views
+ */
+interface UiState
+
+/**
+ * the user actions
+ */
+interface UiEvent
+
+/**
+ * the side effects like error messages which we want to show only once
+ */
+interface UiEffect
